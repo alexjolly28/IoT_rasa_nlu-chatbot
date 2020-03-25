44182b0b0ded4757e04f0d48d6bbc6e6b339844e
alexjolly28 <alexjolly28@gmail.com>
2020-03-26 03:51:47 +0530

    final

--- reqtest.py
+++ reqtest.py
@@ -1,12 +1,26 @@
 import requests
 import json
-
+a={}
 url = 'http://127.0.0.1:5005/model/parse'
-myobj = {"text":"turn on yellow light"}
+myobj = {"text":"turn on  fan into 5  "}
 
 x = requests.post(url, json=myobj)
 
 data=x.text
 json_data=json.loads(data)
-print(type(json_data["intent"]))
-print(json_data["entities"])
+entities=json_data["entities"]
+intents=json_data["intent"]
+acc=intents["confidence"]
+if acc >=.9:
+    device=intents["name"]
+    a["device"]=device
+    for entity in entities:
+        # print((entitie))
+        if entity["entity"]=="action":
+            a["action"]=entity["value"]
+            # print(entitie["value"])
+        if entity["entity"]=="value":
+            a["value"]=entity["value"]
+
+json_a = json.dumps(a)
+print(a)
